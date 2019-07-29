5617ff6e4591075f242500a57e0b20e11fb334ab
Amanda <acbhershey@gmail.com>
2019-07-29 00:20:40 -0400

    Fn added to check is sequence is correct

--- game.js
+++ game.js
@@ -47,3 +47,25 @@ $(".btn").click(function() {
   console.log(userClickedPattern);
   checkAnswer(userClickedPattern.length - 1);
 });
+
+// Checks user sequence against game sequence
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
